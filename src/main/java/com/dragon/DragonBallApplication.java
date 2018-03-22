c2c1f64969adc9130d8a77e8f19e62cf1221d7b9
luojiannan <399129811@qq.com>
2018-03-22 16:50:05 +0800

    first commit

---
+++ src/main/java/com/dragon/DragonBallApplication.java
@@ -0,0 +1,12 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonBallApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dragon/dao/mapper/DemoMapper.java
@@ -0,0 +1,7 @@
+package com.dragon.dao.mapper;
+
+import com.dragon.dao.entity.Demo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface DemoMapper extends Mapper<Demo> {
+}
