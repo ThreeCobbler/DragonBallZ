f9c1e2429b65955a6f63c015de449ff7e622ae6a
luojiannan <399129811@qq.com>
2018-05-04 11:10:04 +0800

    支付接口

---
+++ src/main/java/com/dragon/alipay/trade/page/pay/AliPay.java
@@ -0,0 +1,70 @@
+package com.dragon.alipay.trade.page.pay;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 339939 on 2018/5/3.
+ */
+@RestController
+@RequestMapping("alipay")
+public class AliPay {
+
+    @Value("${APP_PRIVATE_KEY}")
+    private String APP_PRIVATE_KEY;
+
+    @Value("${ALIPAY_PUBLIC_KEY}")
+    private String ALIPAY_PUBLIC_KEY;
+
+    @Value("${SIGN_TYPE}")
+    private String SIGN_TYPE;
+
+    @Value("${AlipayConfig.CHARSET}")
+    private String CHARSET;
+
+    @Value("${AlipayConfig.FORMAT}")
+    private String FORMAT;
+
+    @Value("${APP_ID}")
+    private String APP_ID;
+
+    @RequestMapping("pay")
+    public void doPost(HttpServletRequest httpRequest,
+                       HttpServletResponse httpResponse) throws ServletException, IOException {
+        AlipayClient alipayClient = new DefaultAlipayClient("https://openapi.alipaydev.com/gateway.do",APP_ID, APP_PRIVATE_KEY , FORMAT, CHARSET, ALIPAY_PUBLIC_KEY, SIGN_TYPE); //获得初始化的AlipayClient
+        AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();//创建API对应的request
+        alipayRequest.setReturnUrl("http://domain.com/CallBack/return_url.jsp");
+        alipayRequest.setNotifyUrl("http://domain.com/CallBack/notify_url.jsp");//在公共参数中设置回跳和通知地址
+        alipayRequest.setBizContent("{" +
+                "    \"out_trade_no\":\"20180320010101001\"," +
+                "    \"product_code\":\"FAST_INSTANT_TRADE_PAY\"," +
+                "    \"total_amount\":88.88," +
+                "    \"subject\":\"Iphone6 16G\"," +
+                "    \"body\":\"Iphone6 16G\"," +
+                "    \"passback_params\":\"merchantBizType%3d3C%26merchantBizNo%3d2016010101111\"," +
+                "    \"extend_params\":{" +
+                "    \"sys_service_provider_id\":\"2088511833207846\"" +
+                "    }"+
+                "  }");//填充业务参数
+        String form="";
+        try {
+            form = alipayClient.pageExecute(alipayRequest).getBody(); //调用SDK生成表单
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+        httpResponse.setContentType("text/html;charset=" + CHARSET);
+        httpResponse.getWriter().write(form);//直接将完整的表单html输出到页面
+        httpResponse.getWriter().flush();
+        httpResponse.getWriter().close();
+    }
+}
