9cd5e08dd7bc2c0973bed3ae2c641eada769e6dd
luojiannan <399129811@qq.com>
2018-10-09 09:51:28 +0800

    eureka配置

--- src/main/java/com/dragon/DragonBallApplication.java
+++ src/main/java/com/dragon/DragonBallApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableTransactionManagement
-//@EnableEurekaClient
-//@EnableDiscoveryClient
+@EnableEurekaClient
 @MapperScan(basePackages = "com.dragon.dao.mapper")
 public class DragonBallApplication extends SpringBootServletInitializer {
 
@@ -21,14 +22,5 @@ public class DragonBallApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DragonBallApplication.class, args);
-
-
-//		System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
-//				" ______                    _   ______            \n" +
-//				"|_   _ \\                  / |_|_   _ `.          \n" +
-//				"  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
-//				"  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
-//				" _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
-//				"|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
 	}
 }
