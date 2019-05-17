9e388b0e3364a697d9b21903a45328e7a19c5793
luojiannan <399129811@qq.com>
2019-05-17 16:45:04 +0800

    cat客服端

--- src/main/java/com/dragon/config/CatFilterConfigure.java
+++ src/main/java/com/dragon/config/CatFilterConfigure.java
@@ -1,21 +1,26 @@
 package com.dragon.config;
 
+import com.dianping.cat.servlet.CatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author ljn
  * @date 2019/5/10.
  * cat 配置
  */
-//@Configuration
-//public class CatFilterConfigure {
-//
-//    @Bean
-//    public FilterRegistrationBean catFilter() {
-//        FilterRegistrationBean registration = new FilterRegistrationBean();
-//        CatFilter filter = new CatFilter();
-//        registration.setFilter(filter);
-//        registration.addUrlPatterns("/*");
-//        registration.setName("cat-filter");
-//        registration.setOrder(1);
-//        return registration;
-//    }
-//}
+@Configuration
+public class CatFilterConfigure {
+
+    @Bean
+    public FilterRegistrationBean catFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        CatFilter filter = new CatFilter();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setName("cat-filter");
+        registration.setOrder(1);
+        return registration;
+    }
+}
